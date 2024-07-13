685eb98c43e2702d1669bf2072c570a3952ef9d4
AlejoOrzz <alejoorz12@gmail.com>
2024-07-13 12:54:47 -0600

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,47 +1,59 @@
-<x-guest-layout>
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
+<!DOCTYPE html>
+<html lang="en">
 
-    <form method="POST" action="{{ route('login') }}">
-        @csrf
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ asset('assets/style.css') }}">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Laila:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+    
+    <title>LOGIN</title>
+</head>
 
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Correo Electronico')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
+<body>
+    <section class="contLogin">
+        <article class="contImgs">
+            <div class="logos">
+                <img src="{{url('imgs/logoHelvetas.png')}}" alt="LOGO" />
+                <img src="{{url('imgs/logoCajaLudica.png')}}" alt="LOGO" />
+                <img src="{{url('imgs/logoAsociacionPies.png')}}" alt="LOGO" />
 
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Contraseña')" />
+            </div>
+            <div class="titulo"> <h1><b>COORESPONSABILIDAD DOMESTICA</b></h1></div>
+            <div class="imgPrincipal">
+                <img src="{{url('imgs/imgMain.png')}}" alt="Imagen Principal">
+            </div>
+        </article>
+        <article class="contForm">
+            <div class="tituloLogin">
+                <h1><b>Bienvenidos</b></h1>
+                <h2>Junam Noj</h2>
+            </div>
+            <form method="POST" action="" class="formulario">
+                @csrf                
+                    <div class="mb-4">
+                        <label>Correo Electrónico</label>
+                        <div>
+                            <input type="email" class="form-control" id="Email" placeholder="foxlaj@gmail.com">
+                        </div>
+                    </div>
+                    <div class="mb-4">
+                        <label>Contraseña</label>
+                        <div>
+                            <input type="password" class="form-control" id="inputPassword">
+                        </div>
+                    </div>
+                    <button type="submit">Iniciar Sesión</button>
+            </form>
+            <p><a href="{{url('register')}}">Crear Cuenta</a></p>
+            
+        </article>
+    </section>
+    
+</body>
 
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="current-password" />
-
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
-
-        <!-- Remember Me -->
-        <div class="block mt-4">
-            <label for="remember_me" class="inline-flex items-center">
-                <input id="remember_me" type="checkbox" class="rounded dark:bg-gray-900 border-gray-300 dark:border-gray-700 text-indigo-600 shadow-sm focus:ring-indigo-500 dark:focus:ring-indigo-600 dark:focus:ring-offset-gray-800" name="remember">
-                <span class="ms-2 text-sm text-gray-600 dark:text-gray-400">{{ __('Recuerda mis datos') }}</span>
-            </label>
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('password.request') }}">
-                    {{ __('Recupera tu contraseña?') }}
-                </a>
-            @endif
-
-            <x-primary-button class="ms-3">
-                {{ __('Ingreso') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
+</html>
