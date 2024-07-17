ec36ec0bff6d04b6e368a0c08dc8541aa703f063
Franklin-foshy <53231203+Franklin-foshy@users.noreply.github.com>
2024-07-16 20:40:47 -0600

    asd

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -27,7 +27,7 @@ class AuthenticatedSessionController extends Controller
         $request->authenticate();
 
         $request->session()->regenerate();
-        
+        $user = Auth::user();
         if ($user->rol === 'admin') {
             return redirect()->route('admin.dashboard');
         } elseif ($user->rol === 'participante') {
