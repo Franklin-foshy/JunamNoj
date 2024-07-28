75a46fd61b5ff046eaf5f78e247f01334720588c
KEVIN01306 <kevin60sanchez31@gmail.com>
2024-07-28 07:27:01 -0600

    commit 5

--- public/js/script_nivel1.js
+++ public/js/script_nivel1.js
@@ -1,4 +1,3 @@
-// script.js
 document.addEventListener('DOMContentLoaded', () => {
     const piezas = document.querySelectorAll('.pieza');
     const espacios = document.querySelectorAll('.espacio');
@@ -13,6 +12,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // Función para restablecer las piezas al cargar la página
     function initializePiezas() {
+        console.log('Initializing piezas...');
         piezas.forEach(pieza => {
             // Restablecer estilos y atributos
             pieza.style.position = 'static';
@@ -31,7 +31,7 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     }
 
-    initializePiezas();
+    initializePiezas();  // Llama a initializePiezas al inicio
 
     piezas.forEach(pieza => {
         pieza.setAttribute('data-original-parent', pieza.parentElement.id);
@@ -62,7 +62,7 @@ document.addEventListener('DOMContentLoaded', () => {
             currentX = initialX - xOffset;
             currentY = initialY - yOffset;
 
-
+            setTranslate(currentX, currentY, draggedPiece);
         }
     }
 
@@ -96,7 +96,6 @@ document.addEventListener('DOMContentLoaded', () => {
         });
 
         if (!placed) {
-            // Volver a la posición original si no se coloca en un contenedor
             let originalParent = document.getElementById(draggedPiece.getAttribute('data-original-parent'));
             originalParent.appendChild(draggedPiece);
             draggedPiece.style.position = 'static';
@@ -164,6 +163,7 @@ document.addEventListener('DOMContentLoaded', () => {
         espacio.addEventListener('dragover', allowDrop, false);
         espacio.addEventListener('drop', drop, false);
     });
+
 });
 
 
